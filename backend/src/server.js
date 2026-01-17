66e00476dffd647a65d78523791fead5d22c8665
monjurul-190629 <monjurulalam190629@gmail.com>
2026-01-17 23:54:49 +0600

    message route set up

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,26 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
+
 dotenv.config();
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
+
+// For Auth
+app.use("/api/auth", authRoutes);
+
+// For message
+app.use("/api", messageRoutes);
+
 app.get("/", (req, res) => {
   res.send("Server is running properly");
-});
-
-app.get("/api/signup", (req, res) => {
-  res.send("Signup endpoint");
-});
-
-app.get("/api/login", (req, res) => {
-  res.send("Login endpoint");
-});
-
-app.get("/api/logout", (req, res) => {
-  res.send("Logout endpoint");
 });
 
 app.listen(port, () => {
