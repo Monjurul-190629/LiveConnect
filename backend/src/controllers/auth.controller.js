be7b8203b8f8e8de77282d6bfa03e69f7a2843bd
monjurul-190629 <monjurulalam190629@gmail.com>
2026-03-01 23:50:45 +0600

    feat: check and add avatar for random profile pic

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,24 +1,51 @@
-export const signUp = async (req, res) => {
-  const {email, password, firstName, lastName} = req.body;
+import User from "../models/User.js";
 
-  try{
-    if(!email || !password || !firstName || !lastName){
-      return res.status(400).json({message: "All fields are required"})
+export const signUp = async (req, res) => {
+  const { email, password, firstName, lastName } = req.body;
+
+  try {
+    if (!email || !password || !firstName || !lastName) {
+      return res.status(400).json({ message: "All fields are required" });
     }
 
-    if(password.length < 6){
-      return res.status(400).json({message: "Password should be at least 6 characters"})
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password should be at least 6 characters" });
     }
 
     const emailValidation = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    if(!emailValidation.test(email)){
-      return res.status(400).json({message: "Invalidate email format"})
+    if (!emailValidation.test(email)) {
+      return res.status(400).json({ message: "Invalidate email format" });
     }
 
-  }
-  catch(error){
-    console.log("Error in signup", error)
+    // check for existing email
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail) {
+      return res
+        .status(400)
+        .json({ message: "Email already exists, please use a different one" });
+    }
+
+    // for avatar image
+    const fullName = `${firstName} ${lastName}`;
+
+    const avatar = `https://ui-avatars.com/api/?name=${encodeURIComponent(
+      fullName,
+    )}&size=300&background=random&color=ffffff&rounded=true`;
+
+    // create user
+    const newUser = new User.create({
+      email,
+      firstName,
+      lastName,
+      password,
+      profilePic: avatar
+    })
+    
+  } catch (error) {
+    console.log("Error in signup", error);
   }
 };
 
